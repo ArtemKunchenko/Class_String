666bf23fd084d3fa71e35cb1cba0f0430b82bc65
Артем Кунченко <kunchenko911@ukr.net>
2023-06-11 22:23:46 +0300

    Add project files.

---
+++ Class_String/Source.cpp
@@ -0,0 +1,20 @@
+/*Develop a String class (For now only do the methods from the list), which will be used to work with strings.The class must contain :
+  - default constructor to create a string of 80 characters;
+  - constructor that allows you to create a string of arbitrary size;
+  - a constructor that creates a stringand initializes it with the string received from the user.The class must contain methods for entering lines from the keyboardand displaying lines on the screen.
+  - Copy constructor
+  - int Length() method;
+  - Char StartWith();
+  - Char EndsWith();
+  - Method bool equal(const char*);
+  - char* GetStr();
+  - void SetStr(const char* str);*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	system("pause");
+	return 0;
+}
